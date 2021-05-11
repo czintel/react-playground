75201f616418219e788b935fd09b6d33912e6f3e
Christian Zintel <c.zintel@yahoo.de>
2021-05-11 20:55:05 +0200

    minor js improvements

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import * as React from 'react'
 import Button from './Button'
 import Pagination from './Pagination'
 import Card from './Card'
+import './App.css'
 
 export default () => {
   return (
-    <div>
+    <div className="App">
       <Pagination isLeftDisabled text="1/34" />
       <Pagination text="2/34" />
       <Pagination isRightDisabled text="34/34" />
