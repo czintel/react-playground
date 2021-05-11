8a50efef060878991ae1ee2a7dd410cb389e2d39
Christian Zintel <c.zintel@yahoo.de>
2021-05-11 21:26:56 +0200

    Improve pagination through automation

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,18 @@ import './App.css'
 export default () => {
   return (
     <div className="App">
-      <Pagination isLeftDisabled text="1/34" />
-      <Pagination text="2/34" />
-      <Pagination isRightDisabled text="34/34" />
+      <Pagination currentPage={1} totalPages={34} />
+      <Pagination currentPage={2} totalPages={34} />
+      <Pagination currentPage={34} totalPages={34} />
+
       <Button isActive>Active</Button>
       <Button>Inactive</Button>
+
       <Card
         Heading="Title 1"
         Text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr"
       />
+
       <Card
         Heading="Title 2"
         Text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr"
