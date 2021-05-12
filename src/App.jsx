acda5f1bd9f343e6947fc69a0213e703b9c767fe
Christian Zintel <c.zintel@yahoo.de>
2021-05-12 15:27:35 +0200

    Install uuid

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import * as React from 'react'
-import Button from './Button'
-import Pagination from './Pagination'
-
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import './App.css'
 export default () => {
-  return <div></div>
+  const [size, setSize] = useState(100)
+  const style = {
+    width: size + 'px',
+    height: size + 'px',
+  }
+  return (
+    <div className="App">
+      <label>
+        Size:
+        <input value={size} onChange={handleChange} type="range" max="200" />
+      </label>
+      <div style={style} className="Box" />
+    </div>
+  )
+  function handleChange(event) {
+    const input = event.target
+    setSize(input.value)
+  }
 }
