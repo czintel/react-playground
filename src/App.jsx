7314bea63c6f62d420b93f1b1f39fdc9ee54e028
Christian Zintel <c.zintel@yahoo.de>
2021-05-12 15:22:37 +0200

    Create interactive todo-list with toggleable tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,44 @@
-import * as React from 'react'
-import Card from './Card'
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import './App.css'
-
-const quizData = [
-  {
-    title: 'One big fat title!',
-    question: 'Was ist grün, frisch und klopft an deiner Tür?',
-    answer: 'Ein Klopfsalat',
-    isAnswerVisible: false,
-    isSelected: false,
-    tags: ['gesund', 'langweilig', 'für Nager geeignet'],
-    id: '1',
-  },
-  {
-    title: 'Wow, another big fat title!',
-    question: 'Was ist gelb, laut und lebensgefährlich?',
-    answer: 'Eine Banone',
-    isAnswerVisible: true,
-    isSelected: true,
-    tags: ['gesund', 'krumm', 'süß'],
-    id: '2',
-  },
-]
-
 export default () => {
+  const [todos, setTodos] = useState([])
   return (
     <div className="App">
-      {quizData.map(
-        ({
-          title,
-          question,
-          answer,
-          isAnswerVisible,
-          isSelected,
-          id,
-          tags,
-        }) => (
-          <Card
-            title={title}
-            question={question}
-            answer={answer}
-            isAnswerVisible={isAnswerVisible}
-            isSelected={isSelected}
-            id={id}
-            tags={tags}
-          ></Card>
-        )
-      )}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Add todo:
+          <input name="todo" type="text" />
+        </label>
+        <button>Add</button>
+      </form>
+      <ul>
+        {todos.map(({ text, isDone, id }) => (
+          <li onClick={() => toggleIsDone(id)} key={id}>
+            {text} {isDone && '✅'}
+          </li>
+        ))}
+      </ul>
     </div>
   )
+  function toggleIsDone(id) {
+    const index = todos.findIndex(todo => todo.id === id)
+    const todo = todos[index]
+
+    setTodos([
+      ...todos.slice(0, index),
+      { ...todo, isDone: !todo.isDone },
+      ...todos.slice(index + 1),
+    ])
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const form = event.target
+    const input = form.elements.todo
+    const newTodo = { text: input.value, isDone: false, id: uuidv4() }
+    setTodos([...todos, newTodo])
+    form.reset()
+    input.focus()
+  }
 }
