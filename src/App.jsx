dd2c5fe8ea05b72cfadbc411d3fb7ef620654365
Christian Zintel <c.zintel@yahoo.de>
2021-05-12 15:24:43 +0200

    Setup Modified Boxes

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,5 @@ import Button from './Button'
 import Pagination from './Pagination'
 
 export default () => {
-  return (
-    <div>
-      <Pagination isLeftDisabled text="1/34" />
-      <Pagination text="2/34" />
-      <Pagination isRightDisabled text="34/34" />
-    </div>
-  )
+  return <div></div>
 }
